6d9e59b060dcf89f7153fc45c3100d0d78038683
Cesax69 <cesar20437@cbtis75.edu.mx>
2026-03-12 19:43:30 -0600

    fix(frontend): apuntar a la URL de produccion de railway

--- frontend_pedidos/src/servicios/api.js
+++ frontend_pedidos/src/servicios/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-
 const API = axios.create({
-  baseURL: "http://localhost:3001/api/v1"
+  baseURL: "https://desirable-reprieve-production-76ee.up.railway.app/api/v1"
 });
-
 
 export default API;
